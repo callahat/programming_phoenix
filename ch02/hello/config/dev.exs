9c6842f28cb72c1f0e2ce60d6d9084578b27261b
callahat <tim.callahan25@yahoo.com>
2019-01-23 21:04:29 -0500

    Had to add a user via postgres and give it the CREATEDB priviledge for mix ecto.create to work (after su ing to the postgres user)

--- ch02/hello/config/dev.exs
+++ ch02/hello/config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "elixir",
+  password: "elixir",
   database: "hello_dev",
   hostname: "localhost",
   pool_size: 10
