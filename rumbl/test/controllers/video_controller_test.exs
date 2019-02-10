f9cff9cd3ed0de67e00187a7e80b33756ed22839
callahat <tim.callahan25@yahoo.com>
2019-02-10 16:21:46 -0500

    other integration tests around video counts, and video authorizations

--- rumbl/test/controllers/video_controller_test.exs
+++ rumbl/test/controllers/video_controller_test.exs
@@ -1,10 +1,16 @@
 defmodule VideoControllerTest do
   use Rumbl.ConnCase
 
+  import Plug.Conn, only: [assign: 3]
+
+  alias Rumbl.Video
+  @valid_attrs %{url: "http://youtu.be", title: "vid", description: "a vidya"}
+  @invalid_attrs %{title: "invalid"}
+
   setup %{conn: conn} = config do
     if username = config[:login_as] do
       user = insert_user(%{username: username})
-      conn = Plug.Conn.assign(build_conn(), :current_user, user)
+      conn = assign(build_conn(), :current_user, user)
       {:ok, conn: conn, user: user}
     else
       :ok
@@ -20,6 +26,41 @@ defmodule VideoControllerTest do
     assert html_response(conn, 200) =~ ~r/Listing videos/
     assert String.contains?(conn.resp_body, user_video.title)
     refute String.contains?(conn.resp_body, other_video.title)
+  end
+
+  @tag login_as: "maxheadroom"
+  test "creates user video and redirects", %{conn: conn, user: user} do
+    conn = post conn, video_path(conn, :create), video: @valid_attrs
+    assert redirected_to(conn) == video_path(conn, :index)
+    assert Repo.get_by!(Video, @valid_attrs).user_id == user.id
+  end
+
+  @tag login_as: "maxheadroom"
+  test "does not create video and renders errors when invalid", %{conn: conn, user: _user} do
+    count_before = video_count(Video)
+    conn = post conn, video_path(conn, :create), video: @invalid_attrs
+    assert html_response(conn, 200) =~ "check the errors"
+    assert video_count(Video) == count_before
+  end
+
+  @tag login_as: "maxheadroom"
+  test "authorizes actions against access by other users", %{conn: conn, user: owner} do
+    video = insert_video(owner, @valid_attrs)
+    non_owner = insert_user(%{username: "Slick"})
+    conn = assign(conn, :current_user, non_owner)
+
+    assert_error_sent :not_found, fn ->
+      get(conn, video_path(conn, :show, video))
+    end
+    assert_error_sent :not_found, fn ->
+      get(conn, video_path(conn, :edit, video))
+    end
+    assert_error_sent :not_found, fn ->
+      put(conn, video_path(conn, :update, video, video: @valid_attrs))
+    end
+    assert_error_sent :not_found, fn ->
+      delete(conn, video_path(conn, :delete, video))
+    end
   end
 
   test "requires user authentication on all actions", %{conn: conn} do
@@ -38,4 +79,6 @@ defmodule VideoControllerTest do
     assert html_response(conn, 302)
     assert conn.halted
   end
+
+  defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
 end
