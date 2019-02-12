a20e84a70f6d6998e3baeaed841cb1dd25cf57df
callahat <tim.callahan25@yahoo.com>
2019-02-11 21:23:16 -0500

    sluggin it out

--- rumbl/web/models/video.ex
+++ rumbl/web/models/video.ex
@@ -2,15 +2,23 @@ defmodule Rumbl.Video do
   use Rumbl.Web, :model
 
   @permitted_fields ~w(url title description category_id)
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
 
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
     timestamps()
+  end
+
+  defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
   end
 
   @doc """
@@ -20,6 +28,21 @@ defmodule Rumbl.Video do
     struct
     |> cast(params, @permitted_fields, [])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
